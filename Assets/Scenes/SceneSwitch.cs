58d4aa73b10a0ecc3e43229281bef053c739a72d
Zedong Xun <rhsiun@gmail.com>
2020-12-02 22:17:04 +0800

    aaa

--- Assets/Scenes/SceneSwitch.cs
+++ Assets/Scenes/SceneSwitch.cs
@@ -7,13 +7,13 @@ public class SceneSwitch : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")){
             if(this.CompareTag("House")){
-                SceneManager.LoadScene(1);
+                SceneManager.LoadScene(0);
             }
             if(this.CompareTag("Enemy")){
                 SceneManager.LoadScene(2);
             }
             if(this.CompareTag("Field")) {
-                SceneManager.LoadScene(0);
+                SceneManager.LoadScene(1);
             }
             if(this.CompareTag("Cave")) {
                 SceneManager.LoadScene(3);
